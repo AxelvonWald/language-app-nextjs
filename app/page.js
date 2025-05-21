d26e15f2d9b9abb1d1fd20ac6453751ad6ed9e00
Wizzerhawk <njyoung@disroot.org>
2025-05-22 10:44:45 +1200

    new page and linking buttons added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 // app/page.jsx
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to My Language App</h1>
       <p>This is a Next.js starter.</p>
-      <button>Click Me</button>
+      <Link href="/flashcards">
+        <button>Click Me</button>
+      </Link>
     </main>
   );
 }
